b2a51a26c0bd370a62f5aac6af24419ce044d8a4
taulantn <tn47423@ubt-uni.net>
2022-10-14 03:40:55 +0200

    Added one more package page and style improvements

--- js/app.js
+++ js/app.js
@@ -10,23 +10,23 @@ videoBtn.forEach(btn =>{
 });
 
 var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
-    },
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+  },
 });
 
 var swiper = new Swiper(".brand-slider", {
