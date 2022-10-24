3150e6bab196148c8057b7aed9e1c249e4c10eee
Feston Ahmeti <fa47928@ubt-uni.net>
2022-10-24 21:17:12 +0200

    Changed the design for Book Page

--- js/book.js
+++ js/book.js
@@ -1,4 +1,4 @@
-gsap.set('.main', {position:'fixed', background:'#fff', width:'100%', maxWidth:'100%', height:'100%', top:'68px', left:'50%', x:'-50%'})
+gsap.set('.main-section', {position:'fixed', background:'#fff', width:'100%', maxWidth:'100%', height:'100%', top:'68px', left:'50%', x:'-50%'})
 gsap.set('.scrollDist', {width:'100%', height:'150%'})
 gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:1}})
     .fromTo('.sky', {y:0},{y:-200}, 0)
@@ -8,3 +8,5 @@ gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'botto
     .fromTo('.mountBg', {y:-10},{y:-100}, 0)
     .fromTo('.mountMg', {y:-30},{y:-250}, 0)
     .fromTo('.mountFg', {y:-50},{y:-600}, 0)
+
+    
