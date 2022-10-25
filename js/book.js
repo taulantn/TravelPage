3cef6ba977d0d25affe2b8257b8b02b98eecfd47
Feston Ahmeti <fa47928@ubt-uni.net>
2022-10-25 13:50:01 +0200

    Added elements to Book Page

--- js/book.js
+++ js/book.js
@@ -1,12 +1,26 @@
-gsap.set('.main-section', {position:'fixed', background:'#fff', width:'100%', maxWidth:'100%', height:'100%', top:'68px', left:'50%', x:'-50%'})
-gsap.set('.scrollDist', {width:'100%', height:'150%'})
-gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:1}})
-    .fromTo('.sky', {y:0},{y:-200}, 0)
-    .fromTo('.cloud1', {y:100},{y:-800}, 0)
-    .fromTo('.cloud2', {y:-150},{y:-500}, 0)
-    .fromTo('.cloud3', {y:-50},{y:-650}, 0)
-    .fromTo('.mountBg', {y:-10},{y:-100}, 0)
-    .fromTo('.mountMg', {y:-30},{y:-250}, 0)
-    .fromTo('.mountFg', {y:-50},{y:-600}, 0)
+const sr = ScrollReveal({
+    distance: '25px',
+    duration: 2500
+});
 
-    
+sr.reveal(`.image-left, .popular-destinations-data, 
+.plan-trip-data`, {
+    origin: 'left'
+})
+
+sr.reveal(`.image-center`, {
+    origin: 'bottom'
+})
+
+sr.reveal('.image-right, .showcase-data' ,{
+    origin: 'right'
+})
+
+sr.reveal('.beach-data-wrapper', {
+    origin: 'bottom',
+    interval: 250
+})
+
+sr.reveal('.newsletter-container', {
+    origin: 'top'
+})
